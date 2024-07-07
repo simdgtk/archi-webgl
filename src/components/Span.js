52617c23eb91d98f70620ebf54af05cc1ff54490
Simon Daguet-Kargl <simondaguetkargl@gmail.com>
2024-07-07 13:53:02 +0200

    typo

--- src/components/Span.js
+++ src/components/Span.js
@@ -2,11 +2,11 @@ export default function Span() {
   return (
     <div className="info-span">
       <p className="info-span-desktop">
-        Click on a object from the scene to discover it
+        Click on an object from the scene to discover it
       </p>
       <div className="info-span-mobile">
         <p>
-          The site works better on desktop screen sizes. Click on a object from
+          The site works better on desktop screen sizes. Click on an object from
           the scene to discover it
         </p>
       </div>
